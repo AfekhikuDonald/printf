ee21040e69e1db1a4b66ff4a6da431da58e02abf
Themba B <111020079+103375@users.noreply.github.com>
2022-10-18 16:27:36 +0200

    Update print_long_oct.c

--- print_long_oct.c
+++ print_long_oct.c
@@ -4,7 +4,7 @@
  * prinloct - prints long decimal number in octal
  * @arguments: input number
  * @buf: buffer pointer
- * @ibuf: index for buffer pointer
+ * @ibuf: index for buffer pointer.
  * Return: number of chars printed.
  */
 int prinloct(va_list arguments, char *buf, unsigned int ibuf)
