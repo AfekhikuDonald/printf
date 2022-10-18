00ffdadf78c03dbaaad186b2e7cf5a74e3f2d046
Themba B <111020079+103375@users.noreply.github.com>
2022-10-18 16:29:08 +0200

    Update print_long_unt.c

--- print_long_unt.c
+++ print_long_unt.c
@@ -4,7 +4,7 @@
  * prinlunt - prints a long unsigned integer
  * @arguments: number to print
  * @buf: buffer pointer
- * @ibuf: index for buffer pointer
+ * @ibuf: index for buffer pointer.
  * Return: number of chars printed.
  */
 int prinlunt(va_list arguments, char *buf, unsigned int ibuf)
